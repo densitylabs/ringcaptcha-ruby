9a0b59fcf59bddb88b92056b6d2afd12f65175dc
Federico Ramallo <framallo@gmail.com>
2016-08-31 12:37:33 -0500

    return json when clicking Check RC

--- example/app/assets/javascripts/homes.js
+++ example/app/assets/javascripts/homes.js
@@ -1,17 +1,19 @@
 $(function() {
     $('#btnSubmit').click(function() {
-        var val2 = $("#ringcaptcha_pin_code").val();
-        if (val2.length == 0) {
-            alert("Please enter pin code");
-        } else if (val2.length < 4) {
-            alert("Please enter 4 digit pin code");
-
+        var pinCode = $("input[name='ringcaptcha_pin_code']").first().val();
+        var sessionId = $("input[name='ringcaptcha_session_id']").first().val();
+        if (pinCode.length == 0) {
+            console.log("Please enter pin code");
+        } else if (pinCode.length < 4) {
+            console.log("Please enter 4 digit pin code");
         } else {
             $.ajax({
                 type: 'post',
                 url: '/homes/valid_rc',
-                data: 'param1=' + $("#ringcaptcha_pin_code").val() + '&params2=' + $("#ringcaptcha_session_id").val(),
-                success: function(data) {}
+                data: 'param1=' + pinCode + '&params2=' + sessionId,
+                success: function(data) {
+                  console.log(data);
+                }
             });
         }
     });
--- example/app/controllers/homes_controller.rb
+++ example/app/controllers/homes_controller.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class HomesController < ApplicationController
   require 'ringcaptcha'
   skip_before_filter :verify_authenticity_token
@@ -8,11 +10,12 @@ class HomesController < ApplicationController
   # method to call ringcaptcha methods.
   def valid_rc
     rcap = RingCaptcha::RingCaptcha.new(ENV['RING_CAPTCHA_APP_KEY'], ENV['RING_CAPTCHA_SECRET_KEY'])
-    @rcap_valid = rcap.is_valid?(params[:param1], params[:params2])
-    respond_to do |format|
-      format.js
-    end
 
+    @rcap_valid = rcap.is_valid?(params[:param1], params[:params2])
+
+    pp @rcap_valid
+
+    render json: @rcap_valid
   end
 
 end
